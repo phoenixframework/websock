2e08844f537969f80087a21fd0b4d9a55447b1c0
Mat Trudel <mat@geeky.net>
2022-09-21 15:04:16 -0400

    Version bump to 0.2.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sock.MixProject do
   def project do
     [
       app: :sock,
-      version: "0.2.2",
+      version: "0.2.3",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
