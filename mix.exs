347432c748a7e73b09f84c32c7a735d9545f44af
BobbieBarker <4307099+BobbieBarker@users.noreply.github.com>
2026-08-24 22:05:33 -0700

    Update ExDoc to 0.40.3

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule WebSock.MixProject do
 
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:ex_doc, "~> 0.40.3", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false}
     ]
